7083b3ea0e190734ea38e06c5d1d43f413d7d1ae
tanipu <67771199+yuktnk@users.noreply.github.com>
2021-09-11 18:23:13 +0900

    todo 追加

--- src/index.js
+++ src/index.js
@@ -3,7 +3,38 @@ import "./styles.css";
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  // li 生成
+  const li = document.createElement("li");
+  li.className = "item";
+
+  // p 生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // button （完了）生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button （削除）生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  // li の子要素に各要素を設定
+  li.appendChild(p);
+  li.appendChild(completeButton);
+  li.appendChild(deleteButton);
+
+  console.log(li);
+
+  // 未完了リストに追加
+  document.getElementById("incomplet-list").appendChild(li);
 };
 
 document
